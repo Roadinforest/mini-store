edfa48aa86194ab8a6033a91c92d7f058e390a8f
Roadinforest <whuforest@outlook.com>
2025-09-19 23:07:28 +0800

    feat: 添加用户管理功能，包括获取所有用户和更新用户信息的 API

--- lib/validators.ts
+++ lib/validators.ts
@@ -127,3 +127,9 @@ export const updateProfileSchema = z.object({
 export const updateProductSchema = insertProductSchema.extend({
   id: z.string().min(1, 'Id is required'),
 });
+
+// Schema to update users
+export const updateUserSchema = updateProfileSchema.extend({
+  id: z.string().min(1, 'ID is required'),
+  role: z.string().min(1, 'Role is required'),
+});
