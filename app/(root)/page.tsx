012d841db802362eba301f167b3515f5084ff300
Roadinforest <whuforest@outlook.com>
2025-09-20 12:02:51 +0800

    feat: 在首页添加倒计时组件和图标框，展示促销信息和服务优势

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@ import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const HomePage = async () => {
   const latesProducts = await getLatestProducts();
@@ -12,6 +14,8 @@ const HomePage = async () => {
       {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latesProducts} title="Newest Arrivals" limit={6} />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
