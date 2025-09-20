6dde15a249abd563725ec62073b6e0d5c4771434
Roadinforest <whuforest@outlook.com>
2025-09-20 08:13:57 +0800

    feat：首页面添加 products carousel 、category-drawer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 
 const HomePage = async () => {
   const latesProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <>
+      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latesProducts} title="Newest Arrivals" limit={6} />
     </>
   );
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoriesDrawer from "./category-drawer";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          {/* <CategoriesDrawer /> */}
+          <CategoriesDrawer />
           <Link href="/" className="flex-start ml-4">
             <Image
               priority={true}
